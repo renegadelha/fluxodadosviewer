c314fadde1b970167cdbf9c1996467bcb70c3890
renegadelha <renegadelha@gmail.com>
2025-01-25 10:28:30 -0300

    atualizei o backend

--- Servidor.py
+++ Servidor.py
@@ -3,7 +3,9 @@ from flask_socketio import SocketIO
 import random
 
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode='threading')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='threading')
+
+#socketio = SocketIO(app, async_mode='threading')
 
 @app.route('/')
 def index():
