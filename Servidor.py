276fcacc1b065df4d71f6bcd4635e62cb8a7c33a
renegadelha <renegadelha@gmail.com>
2025-01-25 10:10:37 -0300

    first commit

---
+++ Servidor.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import random
+
+app = Flask(__name__)
+socketio = SocketIO(app, async_mode='threading')
+
+@app.route('/')
+def index():
+    return render_template('index2.html')
+
+def gerar_dados():
+    while True:
+        temp = random.uniform(20.0, 30.0)
+        socketio.emit('temperatura', {'value': round(temp, 2)})
+        socketio.sleep(1)
+
+
+if __name__ == '__main__':
+    socketio.start_background_task(gerar_dados)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
